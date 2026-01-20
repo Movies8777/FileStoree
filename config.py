5f77d1f11ddc63fc8825757a03d2db086343d3ae
Movies8777 <roxedits07@gmail.com>
2026-01-20 17:28:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,12 +25,12 @@ API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003646933017")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "♚𝖧υηтєʀ⚰️") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "8435672368")) # Owner id
+OWNER_ID = int(os.environ.get("OWNER_ID", "6243694287")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://hosoy42933:cgAc2eQk22JieEWn@cluster0.hb9xzjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "ad_database")
+DB_NAME = os.environ.get("DATABASE_NAME", "ad_databasebjmkn")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "120"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Spicylinebun")
